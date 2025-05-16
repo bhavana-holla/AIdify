3a4d59f5112591c0f6774be6949bde6ad1834075
bhavana-holla <65540128+bhavana-holla@users.noreply.github.com>
2025-05-16 07:38:24 +0530

    Create chat_bubble.dart

---
+++ aidify/lib/widgets/chat_bubble.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ChatBubble extends StatelessWidget {
+  final String message;
+  final bool isUser;
+
+  const ChatBubble({super.key, required this.message, required this.isUser});
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: isUser ? Alignment.centerRight : Alignment.centerLeft,
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: isUser ? Colors.blue.shade100 : Colors.red.shade100,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width * 0.75),
+        child: Text(
+          message,
+          style: const TextStyle(fontSize: 16),
+        ),
+      ),
+    );
+  }
+}
